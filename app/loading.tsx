56c619364df6738c2b03311f2afddf02e2f9ff34
Bethrand Nnaemeka <bethrand2019@gmail.com>
2025-04-22 15:43:41 +0100

    [FIX]: update Loading component to use full screen height and background color

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <main className="text-5xl h-full flex items-center justify-center">
+    <main className="text-5xl h-screen bg-muted flex items-center justify-center">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
       {/* <h1>Loading...</h1> */}
     </main>
